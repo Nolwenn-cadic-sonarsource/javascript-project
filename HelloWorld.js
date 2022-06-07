94fa23fe2ed55560a348ef3d94b5ebd2ba688b16
Nolwenn Cadic <nolwenn.cadic@sonarsource.com>
2022-06-07 16:55:59 +0200

    Add new Hello World line

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,16 +1,3 @@
-<!DOCTYPE HTML>
-<html>
 
-<body>
-
-  <p>Before the script...</p>
-
-  <script>
-    alert( 'Hello, world!' );
-  </script>
-
-  <p>...After the script.</p>
-
-</body>
-
-</html>
+alert( 'Hello, world!' );
+alert( 'Hello, world again! ' );
